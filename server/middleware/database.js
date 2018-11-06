db0873159e44648c61b4bf16fd29fd184154e1a7
manuelnelson <elnels@gmail.com>
2018-11-06 14:35:56 -0500

    fix a few items

--- server/middleware/database.js
+++ server/middleware/database.js
@@ -1,9 +1,11 @@
+let config = require('dotenv').config();
 const { Client } = require('pg')
+console.log(config.parsed.PGDATABASE)
 const client = new Client({
-  user: 'readonly',
-  host: 'boat.colorado.edu',
-  database: 'vbd23',
-  password: 'viirs2018',
+  user: config.parsed.PGUSER,
+  host: config.parsed.PGHOST,//'boat.colorado.edu',
+  database: config.parsed.PGDATABASE,//'vbd23',
+  password: config.parsed.PGPASSWORD,//'viirs2018',
   port: 5432,
 }
 )
